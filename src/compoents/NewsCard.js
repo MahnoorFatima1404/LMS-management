b973288952cb1af9285adc86d07425957741a9d7
MahnoorFatima1404 <116874131+MahnoorFatima1404@users.noreply.github.com>
2023-06-12 11:45:32 +0500

    Update NewsCard

--- src/compoents/NewsCard.js
+++ src/compoents/NewsCard.js
@@ -14,11 +14,9 @@ function NewsCard(props) {
             </div>
             <div class="col-md-8">
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Scholarships</h5>
                 <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                ELIT DUIED AIUSMOD TEMPOR DID LABORE DOLORY AIUSMOD TEMPOR DID.
                 </p>
                 <p class="card-text">
                   <small class="text-muted">Last updated 3 mins ago</small>
